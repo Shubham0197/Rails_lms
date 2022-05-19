b5cb47b0ab98665171e419252c6fbe7f80367a12
Shubham0197 <shubhamjain0197@gmail.com>
2022-05-19 20:04:10 +0530

    Adding Foreign Key In Books Table From Publishers Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_19_155641) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_19_155642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_155641) do
     t.string "address"
   end
 
+  add_foreign_key "books", "publishers"
 end
